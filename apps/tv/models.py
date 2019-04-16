15dc5fafb3cb73886598ebbdf12795a69b0a72a6
mmpearce <mary.milburn.pearce@gmail.com>
2019-04-16 15:12:28 -0700

    moved validation to models and added persistence to invalid user input values for add and edit

--- apps/tv/models.py
+++ apps/tv/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from datetime import datetime
+
+class TVShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 1:
+            errors["title"] = "Title cannot be blank"
+        if len(postData['network']) < 1:
+            errors["network"] = "Network cannot be blank"
+        if len(postData['rel_date']) < 1:
+            errors["rel_date"] = "Release Date cannot be blank"
+        else:
+            d1 = datetime.strptime(postData['rel_date'], "%Y-%m-%d").date()
+            if d1 > datetime.now().date():
+                print("greater than today")
+                errors["future_rel_date"] = "Release Date must today or in the past"
+        if len(postData['desc']) > 0 and len(postData['desc']) < 10:
+            errors["desc"] = "Description must be longer than 10 characters if present"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=100)
     network = models.CharField(max_length=50)
     release_date = models.DateField()
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    objects = TVShowManager()
 
     def __repr__(self):
-        return '%s | %s | %s | %s' % (self.title, self.network, self.release_date, self.description)
+        return f"Title: {self.title}, Network: {self.network}, Released: {self.release_date}, Description: {self.description}"
--- apps/tv/views.py
+++ apps/tv/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import Show
 from django.db.models import Q
+from django.contrib import messages
 import datetime
 
 def index(request):
@@ -14,6 +15,7 @@ def all_shows(request):
 # ("/users/<user_id>") url(r'^shows/(?P<show_id>\d+)$', views.display_show),      
 def display_show(request, show_id):
     this_show=Show.objects.get(id=show_id)
+    print(this_show.__repr__())
     context = { "show" : this_show }
     return render(request, "tv/show_show.html", context)
 
@@ -23,34 +25,28 @@ def new_show(request):
 
 # ("/users/create", methods=["POST"])  url(r'^shows/create$', views.create_show),
 def create_show(request):     
-    is_valid=True     
-    context = { 'error_msgs' : [] }
-    if len(request.POST['title']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Title cannot be blank")
-    if len(request.POST['network']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Network cannot be blank")
-    if len(request.POST['rel_date']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Release Date cannot be blank")
-    if len(request.POST['desc']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Description cannot be blank")
-    if is_valid:
-        req_title=request.POST['title']
-        req_network=request.POST['network']
-        req_rel_date = datetime.datetime.strptime(request.POST['rel_date'], '%Y-%m-%d')
-        req_desc=request.POST['desc']
-        new_show=Show.objects.create(title=req_title, network=req_network, \
-                release_date=req_rel_date, description=req_desc)
-        if not new_show:
-            is_valid=False
-            context['error_msgs'].append("Could not add this TV Show")
-    if is_valid:
-        return redirect(f"/shows/{new_show.id}")
-    else:
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        this_show=Show()
+        this_show.title=request.POST['title']
+        this_show.network=request.POST['network']
+        this_show.description=request.POST['desc']
+        context = { 
+            "show" : this_show,
+            "time" : request.POST['rel_date'] }
         return render(request, "tv/add_show.html", context)
+    req_title=request.POST['title']
+    req_network=request.POST['network']
+    req_rel_date = datetime.datetime.strptime(request.POST['rel_date'], '%Y-%m-%d')
+    req_desc=request.POST['desc']
+    new_show=Show.objects.create(title=req_title, network=req_network, \
+        release_date=req_rel_date, description=req_desc)
+    if not new_show:
+        messages.error(request, "Could not add this TV Show")
+        return render(request, "tv/add_show.html")
+    return redirect(f"/shows/{new_show.id}")
 
 # ("/users/<user_id>/edit") url(r'^users/(?P<id>\d+)/edit$', views.edit_show),
 def edit_show(request, show_id):         
@@ -63,30 +59,25 @@ def edit_show(request, show_id):
 
 # ("/users/<user_id>/update", methods=["POST"]) url(r'^users/(?P<id>\d+)/update$', views.update_show),         
 def update_show(request, show_id):       
-    is_valid=True     
-    context = { 'error_msgs' : [] }
-    if len(request.POST['title']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Title cannot be blank")
-    if len(request.POST['network']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Network cannot be blank")
-    if len(request.POST['rel_date']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Release Date cannot be blank")
-    if len(request.POST['desc']) < 1:
-        is_valid=False
-        context['error_msgs'].append("Description cannot be blank")
-    if is_valid:
-        this_show=Show.objects.get(id=show_id)
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        this_show=Show()
         this_show.title=request.POST['title']
         this_show.network=request.POST['network']
-        this_show.release_date=datetime.datetime.strptime(request.POST['rel_date'], '%Y-%m-%d')
         this_show.description=request.POST['desc']
-        success=this_show.save()  
-        return redirect("/shows/" + show_id)
-    else:
+        context = { 
+            "show" : this_show,
+            "time" : request.POST['rel_date'] }
         return render(request, "tv/edit_show.html", context)
+    this_show=Show.objects.get(id=show_id)
+    this_show.title=request.POST['title']
+    this_show.network=request.POST['network']
+    this_show.release_date=datetime.datetime.strptime(request.POST['rel_date'], '%Y-%m-%d')
+    this_show.description=request.POST['desc']
+    this_show.save()  
+    return redirect("/shows/" + show_id)
 
 # ("/users/<user_id>/destroy") url(r'^users/(?P<id>\d+)/destroy$', views.delete_show),
 def delete_show(request, show_id):       
